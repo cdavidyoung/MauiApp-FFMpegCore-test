f1eb035e044b8af80cbaafdf3c4b34d17c780967
charles david young <charlesdavidyoung@gmail.com>
2023-03-09 05:26:20 -0700

    pick test

--- MauiApp FFMpegCore test/MainPage.xaml.cs
+++ MauiApp FFMpegCore test/MainPage.xaml.cs
@@ -21,6 +21,8 @@ public partial class MainPage : ContentPage
       else
          CounterBtn.Text = $"Clicked {count} times";
 
+        pickTest();
+        
         try
         {
             FFMpegHelper.VerifyFFMpegExists(new FFOptions());
@@ -32,5 +34,59 @@ public partial class MainPage : ContentPage
 
         SemanticScreenReader.Announce(CounterBtn.Text);
    }
-}
 
+   public async Task<FileResult> PickAndShow(PickOptions options)
+   {
+      try
+      {
+         var result = await FilePicker.PickAsync(options);
+         if (result != null)
+            CounterBtn.Text = $"PickAndShow {result.FileName}";
+        
+         return result;
+      }
+      catch (Exception ex)
+      {
+         CounterBtn.Text = $"PickAndShow {ex}";
+      }
+    
+      return null;
+   }
+
+   public async Task pickTest()
+   {
+      try
+      {
+         // get images from zip file
+         // https://android.googlesource.com/platform/external/mime-support/+/fa3f892f28db393b1411f046877ee48179f6a4cf/mime.types
+         // https://developer.apple.com/documentation/uniformtypeidentifiers/uttypearchive?changes=_9
+         var zipFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                                                  {
+                                                     { DevicePlatform.iOS, new[] { "public.archive" } },
+                                                     { DevicePlatform.Android, new[] { "application/zip" } },
+                                                     { DevicePlatform.WinUI, new[] { "zip" } },
+                                                     { DevicePlatform.MacCatalyst, new[] { "zip" } },
+                                                        });
+         var optionsZip = new PickOptions
+            {
+               PickerTitle = "Please select a previously backed up zip file",
+               FileTypes = zipFileType
+            };
+
+         string extractPath;
+         var zipFile = await PickAndShow(optionsZip);
+         if (zipFile != null)
+         {
+            CounterBtn.Text = $"Pick success: {zipFile.FullPath}";
+         }
+      }
+      catch (OperationCanceledException ex)
+      {
+         CounterBtn.Text = $"pickTest {ex}";
+      }
+      catch (Exception ex)
+      {
+         CounterBtn.Text = $"pickTest {ex}";
+      }
+   }
+}
